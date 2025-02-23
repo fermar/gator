d76dfc8afecdb2028df341e0018f6fd3dad663e9
fermar <fermar.ingenieria@gmail.com>
2025-02-23 03:36:52 +0000

    ajuste menor

--- commands.go
+++ commands.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type command struct {
 	name string
 	args []string
@@ -16,7 +18,10 @@ func (c *commands) register(name string, f func(*state, command) error) error {
 }
 
 func (c *commands) run(s *state, cmd command) error {
-	f := c.comandos[cmd.name]
+	f, ok := c.comandos[cmd.name]
+	if !ok {
+		return fmt.Errorf("El comando %v no existe", cmd.name)
+	}
 	err := f(s, cmd)
 	if err != nil {
 		return err
